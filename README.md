1f03cb0a229ce6e4883d44c9473531fda37da3b6
Gregor Sturm <mail@gregor-sturm.de>
2018-02-28 14:23:09 +0100

    rephrase quanTIseq devonvolution as an R function

---
+++ README.md
@@ -0,0 +1,7 @@
+# quanTIseq in R
+
+Wrap the deconvolution part of `quanTIseq` ([Finotello F. et al, 2017](https://doi.org/10.1101/223180)) into an
+R function. The R function can be directly called with a gene expression matrix.
+
+* See [test_quantiseq.Rmd](test_quantiseq.Rmd) for usage examples.
+* See [quanTIseq_decon.R](quanTIseq_decon.R) for the implementation of the function.
--- quanTIseq_decon.R
+++ quanTIseq_decon.R
@@ -1,237 +1,209 @@
-args <- commandArgs(TRUE)
-
-# parameters:
-# mix.mat.file <- args[1]
-# output <- args[2]
-# expr <- args[3]
-# arrays <- args[4]
-# signame <- args[5]
-# tumor <- args[6]
-# mRNAscale <- args[7]
-# method <- args[8]
-# prefix <- args[9]
-# btotalcells <- args[10]
-# rmgenes <- args[11]
-
-# Input file
-mix.mat.file <- "examples/quanTIseqTest_gene_tpm.txt"
-
-# --outputdir: 	path to the output directory. 
-#               Default:  current directory.
-output <- "./"
-
-# not used anywhere!?
-expr <- args[3]
-
-# --arrays: 		specifies whether expression data are from microarrays (instead of RNA-seq). 
-#               If TRUE, the "--rmgenes" parameter is set to "none". 
-#               Default: FALSE.
-arrays <- FALSE
-
-signame <- "TIL10"
-
-# --tumor: 		specifies whether expression data are from tumor samples. If TRUE, signature genes 
-#             with high expression in tumor samples are removed (see [1]). 
-#             Default: FALSE.
-tumor <- FALSE
-
-# --mRNAscale: 	specifies whether cell fractions must be scaled to account for cell-type-specific 
-#               mRNA content. 
-#               Default: TRUE.
-mRNAscale <- TRUE
-
-# --method: 		deconvolution method to be used: "hampel", "huber", or "bisquare" for robust regression 
-#               with Huber, Hampel, or Tukey bisquare estimators, respectively, or "lsei" for constrained 
-#               least squares regression. The fraction of uncharacterized cells ("other") is computed only 
-#               by the "lsei" method. 
-#               Default: "lsei".
-method <- "lsei"
-
-# --prefix: 		prefix of the output files. 
-#               Default: "quanTIseq".
-prefix <- "quanTIseq"
-
-# compute cell density
-btotalcells <- FALSE
-
-# --rmgenes: 		specifies which genes must be removed from the signature matrix before running
-#                 deconvolution. This parameter can be equal to:
-# 
-#                   - "none": no genes are removed;
-#                 - "default": the list of genes reported in this file are removed;
-#                 - A path to a text file containing a list of genes to be removed can be specified.
-#                 The text file must report one gene symbol per line.
-# 
-#                 We advise using "default" and "none" for the deconvolution of RNA-seq and microarray data,
-#                 respectively.
-#                 Default: "default".
-rmgenes <- "unassigned"
-
-
-
-library(preprocessCore)
-
 source("DOCKER_codes.R")
 
-message("\nRunning quanTIseq deconvolution module\n")
+#' Use quanTIseq (F. Finotello, C. Mayer, C. Plattner, G. Laschober, D. Rieder, 
+#' H. Hackl, A. Krogsdam, W. Posch, D. Wilflingseder, S. Sopper, M. Jsselsteijn,
+#' D. Johnsons, Y. Xu, Y. Wang, M. E. Sanders, M. V. Estrada, P. Ericsson-Gonzalez,
+#' J. Balko, N. F. de Miranda, Z. Trajanoski. "quanTIseq: quantifying immune contexture of human tumors".
+#' bioRxiv 223180. https://doi.org/10.1101/223180.) to deconvolute a gene expression matrix. 
+#' 
+#' @param mix.mat table with the gene TPM (or microarray expression values) for all samples to be deconvoluted
+#'     (Gene symbols on the first column and sample IDs on the first row). Expression data must be on non-log scale
+#' @param arrays specifies whether expression data are from microarrays (instead of RNA-seq). 
+#'     If TRUE, the "--rmgenes" parameter is set to "none".
+#' @param signame name of the signature matrix. Currently only `TIL10` is available. 
+#' @param tumor 	specifies whether expression data are from tumor samples. If TRUE, signature genes 
+#'     with high expression in tumor samples are removed (see [1]). 
+#'     Default: FALSE.
+#' @param mRNAscale specifies whether cell fractions must be scaled to account for cell-type-specific 
+#'     mRNA content. 
+#'     Default: TRUE.
+#' @param method deconvolution method to be used: "hampel", "huber", or "bisquare" for robust regression 
+#'     with Huber, Hampel, or Tukey bisquare estimators, respectively, or "lsei" for constrained 
+#'     least squares regression. The fraction of uncharacterized cells ("other") is computed only 
+#'     by the "lsei" method. 
+#'     Default: "lsei".
+#' @param btotalcells compute cell densities instead of fractions
+#'     Default: FALSE
+#' @param rmgenes Default: "default" for RNAseq, "none" for microArray data
+quantiseq_decon = function(mix.mat,
+                           arrays=FALSE,
+                           signame="TIL10",
+                           tumor=FALSE,
+                           mRNAscale=TRUE,
+                           method="lsei",
+                           btotalcells=FALSE,
+                           rmgenes="unassigned") {
 
-# List of genes to be discarded
-if (rmgenes=="unassigned" && arrays==TRUE) { # For Microarrays
-  rmgenes<-"none"
+  library(preprocessCore)
   
-} else if (rmgenes=="unassigned" && arrays==FALSE) { # For RNA-seq
-  rmgenes<-"default"
+  message("\nRunning quanTIseq deconvolution module\n")
   
-}
-
-
-# Files
-listsig<-c("TIL10")
-if (signame %in% listsig) {
-  
-  sig.mat.file<-file.path("home/deconvolution/",
-                          paste0(signame, "_signature.txt"))
-  
-  mRNA.file<-file.path("home/deconvolution/",
-                       paste0(signame, "_mRNA_scaling.txt"))
-  
-  fileab<-file.path("home/deconvolution/",
-    paste0(signame,"_TCGA_aberrant_immune_genes.txt"))
-  
-  if (rmgenes=="default") {
-    filerm<-file.path("home/deconvolution/",
-                      paste0(signame,"_rmgenes.txt"))
+  # List of genes to be discarded
+  if (rmgenes=="unassigned" && arrays==TRUE) { # For Microarrays
+    rmgenes<-"none"
     
-  } else if (rmgenes=="path") {
-    filerm<-file.path("home/deconvolution/",
-                      paste0(signame,"rmgenes.txt"))
+  } else if (rmgenes=="unassigned" && arrays==FALSE) { # For RNA-seq
+    rmgenes<-"default"
     
   }
-
   
-} else {
   
-  sig.mat.file<-paste0(signame, "_signature.txt")
-  mRNA.file<-paste0(signame, "_mRNA_scaling.txt")
- 
-}
-
-# Load mixture matrix
-mix.mat<-read.table(mix.mat.file, header=TRUE, sep="\t", row.names=1)
-
-if(is.numeric(mix.mat[1,1])!= TRUE){
-  stop("Wrong input format for the mixture matrix! Please follow the instructions of the documentation.")
-}
-
-# Load signature 
-sig.mat<-read.table(sig.mat.file, header=TRUE, sep="\t", row.names=1)
-
-# Load normalization factors (set all to 1 if mRNAscale==FALSE)
-if (mRNAscale) {
-
-  mRNA<-read.table(mRNA.file, 
-                   sep="\t", 
-                   header=FALSE, 
-                   stringsAsFactors=FALSE)
-  colnames(mRNA)<-c("celltype", "scaling")
-  mRNA<-as.vector(as.matrix(mRNA$scaling[match(colnames(sig.mat), mRNA$celltype)]))
-  
-} else {
-  
-  mRNA<-rep(1, ncol(sig.mat))
-  
-}
-
-# Preprocess mixture matrix
-message(paste0("Gene expression normalization and re-annotation (arrays: ", 
-  arrays, ")\n"))
-mix.mat<-fixMixture(mix.mat, arrays=arrays)
-
-# Remove noisy genes 
-if (rmgenes!="none") {
-  
+  # Files
+  listsig<-c("TIL10")
   if (signame %in% listsig) {
     
-    lrmgenes<-as.vector(read.table(filerm, header=FALSE, sep="\t")[,1])
+    sig.mat.file<-file.path("home/deconvolution/",
+                            paste0(signame, "_signature.txt"))
+    
+    mRNA.file<-file.path("home/deconvolution/",
+                         paste0(signame, "_mRNA_scaling.txt"))
+    
+    fileab<-file.path("home/deconvolution/",
+      paste0(signame,"_TCGA_aberrant_immune_genes.txt"))
+    
+    if (rmgenes=="default") {
+      filerm<-file.path("home/deconvolution/",
+                        paste0(signame,"_rmgenes.txt"))
+      
+    } else if (rmgenes=="path") {
+      filerm<-file.path("home/deconvolution/",
+                        paste0(signame,"rmgenes.txt"))
+      
+    }
+  
+    
+  } else {
+    
+    sig.mat.file<-paste0(signame, "_signature.txt")
+    mRNA.file<-paste0(signame, "_mRNA_scaling.txt")
+   
+  }
+  
+  if(is.numeric(mix.mat[[1,1]])!= TRUE){
+    stop("Wrong input format for the mixture matrix! Please follow the instructions of the documentation.")
+  }
+  
+  # Load signature 
+  sig.mat<-read.table(sig.mat.file, header=TRUE, sep="\t", row.names=1)
+  
+  # Load normalization factors (set all to 1 if mRNAscale==FALSE)
+  if (mRNAscale) {
+  
+    mRNA<-read.table(mRNA.file, 
+                     sep="\t", 
+                     header=FALSE, 
+                     stringsAsFactors=FALSE)
+    colnames(mRNA)<-c("celltype", "scaling")
+    mRNA<-as.vector(as.matrix(mRNA$scaling[match(colnames(sig.mat), mRNA$celltype)]))
+    
+  } else {
+    
+    mRNA<-rep(1, ncol(sig.mat))
+    
+  }
+  
+  # Preprocess mixture matrix
+  message(paste0("Gene expression normalization and re-annotation (arrays: ", 
+    arrays, ")\n"))
+  mix.mat<-fixMixture(mix.mat, arrays=arrays)
+  
+  # Remove noisy genes 
+  if (rmgenes!="none") {
+    
+    if (signame %in% listsig) {
+      
+      lrmgenes<-as.vector(read.table(filerm, header=FALSE, sep="\t")[,1])
+      n1<-nrow(sig.mat)
+      sig.mat<-sig.mat[!rownames(sig.mat) %in% lrmgenes,, drop=FALSE]
+      n2<-nrow(sig.mat)
+      message(paste0("Removing ", n1-n2, " noisy genes\n"))
+      
+    }
+  }
+  
+  # Fix tumor data
+  if (tumor) {
+    
+    if (signame %in% listsig) {
+  
+    abgenes<-as.vector(read.table(fileab, header=FALSE, sep="\t")[,1])
     n1<-nrow(sig.mat)
-    sig.mat<-sig.mat[!rownames(sig.mat) %in% lrmgenes,, drop=FALSE]
+    sig.mat<-sig.mat[!rownames(sig.mat) %in% abgenes,, drop=FALSE]
     n2<-nrow(sig.mat)
-    message(paste0("Removing ", n1-n2, " noisy genes\n"))
+    message(paste0("Removing ", n1-n2, " genes with high expression in tumors\n"))
     
+    }
   }
-}
-
-# Fix tumor data
-if (tumor) {
   
-  if (signame %in% listsig) {
-
-  abgenes<-as.vector(read.table(fileab, header=FALSE, sep="\t")[,1])
-  n1<-nrow(sig.mat)
-  sig.mat<-sig.mat[!rownames(sig.mat) %in% abgenes,, drop=FALSE]
-  n2<-nrow(sig.mat)
-  message(paste0("Removing ", n1-n2, " genes with high expression in tumors\n"))
+  # Signature genes present in the mixture
+  ns<-nrow(sig.mat)
+  us<-length(intersect(rownames(sig.mat), rownames(mix.mat)))
+  perc<-round(us*100/ns,digits=2)
+  message(paste0("Signature genes found in data set: ", 
+    us, "/", ns, " (", perc, "%)\n"))
+  
+  # Run deconvolution
+  message(paste0("Mixture deconvolution (method: ", method, ")\n"))
+  results1<-quanTIseq(sig.mat,
+                     mix.mat,
+                     scaling=mRNA,
+                     method=method)
+  if ("Tregs" %in% colnames(sig.mat) && "T.cells.CD4" %in% colnames(sig.mat) && method %in% c("lsei")) {
+  
+    minTregs<-0.02
+    i<-which(colnames(sig.mat)=="T.cells.CD4")
+    results2<-quanTIseq(sig.mat[,-i],
+      mix.mat,
+      scaling=mRNA[-i],
+      method=method)
+  
+    ind<-which(results1[,"Tregs"]<minTregs)
+    if (length(ind)>0) {
+  
+      results1[ind,"Tregs"]<-(results2[ind,"Tregs"]+results1[ind,"Tregs"])/2
+      results1[ind,"T.cells.CD4"]<-pmax(0,results1[ind,"T.cells.CD4"]-(results2[ind,"Tregs"]+results1[ind,"Tregs"])/2)
+  
+    }
   
   }
-}
-
-# Signature genes present in the mixture
-ns<-nrow(sig.mat)
-us<-length(intersect(rownames(sig.mat), rownames(mix.mat)))
-perc<-round(us*100/ns,digits=2)
-message(paste0("Signature genes found in data set: ", 
-  us, "/", ns, " (", perc, "%)\n"))
-
-# Run deconvolution
-message(paste0("Mixture deconvolution (method: ", method, ")\n"))
-results1<-quanTIseq(sig.mat,
-                   mix.mat,
-                   scaling=mRNA,
-                   method=method)
-if ("Tregs" %in% colnames(sig.mat) && "T.cells.CD4" %in% colnames(sig.mat) && method %in% c("lsei")) {
-
-  minTregs<-0.02
-  i<-which(colnames(sig.mat)=="T.cells.CD4")
-  results2<-quanTIseq(sig.mat[,-i],
-    mix.mat,
-    scaling=mRNA[-i],
-    method=method)
-
-  ind<-which(results1[,"Tregs"]<minTregs)
-  if (length(ind)>0) {
-
-    results1[ind,"Tregs"]<-(results2[ind,"Tregs"]+results1[ind,"Tregs"])/2
-    results1[ind,"T.cells.CD4"]<-pmax(0,results1[ind,"T.cells.CD4"]-(results2[ind,"Tregs"]+results1[ind,"Tregs"])/2)
-
-  }
-
-}
-results<-results1
-results<-results/apply(results,1,sum)
-
-# Save results using user's output ID
-fileout<-paste0(output, prefix, "_cell_fractions.txt")
-DCres <- results
-results<-cbind(rownames(results), results)
-colnames(results)[1]<-"Sample"
-write.table(results, 
-            sep="\t",
-            row.names=FALSE,
-            quote=FALSE,
-            file=fileout)
-
-if (btotalcells == TRUE){
+  results<-results1
+  results<-results/apply(results,1,sum)
   
-  celldens <- celldensities(DCres)
-  fileout2 <- paste0(output, prefix, "_cell_densities.txt")
-  celldens<-cbind(rownames(celldens), celldens)
-  colnames(celldens)[1]<-"Sample"
-  write.table(celldens,
-              sep="\t",
-              row.names=FALSE,
-              quote=FALSE,
-              file=fileout2)
+  message("Deconvolution sucessful!")
+  
+  
+  
+  # Save results using user's output ID
+  DCres <- results
+  
+  
+  if (btotalcells == TRUE) {
+    celldens <- data.frame(celldensities(DCres))
+    # fileout2 <- paste0(output, prefix, "_cell_densities.txt")
+    celldens<-cbind(rownames(celldens), celldens)
+    colnames(celldens)[1]<-"Sample"
+    return(celldens)
+    # write.table(celldens,
+    #             sep="\t",
+    #             row.names=FALSE,
+    #             quote=FALSE,
+    #             file=fileout2)
+  } else {
+    # fileout<-paste0(output, prefix, "_cell_fractions.txt")
+    # cast to dataframe, otherwise cbind will cast to character. 
+    results = data.frame(results)
+    results<-cbind(rownames(results), results)
+    colnames(results)[1]<-"Sample"
+    return(results)
+    # write.table(results, 
+    #             sep="\t",
+    #             row.names=FALSE,
+    #             quote=FALSE,
+    #             file=fileout)
+  }
+  
+
+
 }
 
-message("Deconvolution results saved to file!")
 
